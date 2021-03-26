208fd1c7187e2bbac31bd8fe83194a83d8d3a883
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-03-26 14:41:48 +0100

    feat: bump Jinja2 from 2.10.1 to 2.11.3 to fix security vulnerability

--- layers/layer2_python2/0550_bjoern/Makefile.mk
+++ layers/layer2_python2/0550_bjoern/Makefile.mk
@@ -11,8 +11,8 @@ Fast And Ultra-Lightweight HTTP/1.1 WSGI Server
 WEBSITE=https://github.com/thefab/bjoern/tree/metwork
 LICENSE=BSD
 
-all:: $(PYTHON2_SITE_PACKAGES)/$(NAME)-3.0.0-py$(PYTHON2_SHORT_VERSION)-linux-x86_64.egg/bjoern.py
-$(PYTHON2_SITE_PACKAGES)/$(NAME)-3.0.0-py$(PYTHON2_SHORT_VERSION)-linux-x86_64.egg/bjoern.py:
+all:: $(PYTHON2_SITE_PACKAGES)/$(NAME)-3.0.0-py$(PYTHON2_SHORT_VERSION)-linux-x86_64.egg
+$(PYTHON2_SITE_PACKAGES)/$(NAME)-3.0.0-py$(PYTHON2_SHORT_VERSION)-linux-x86_64.egg:
 	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard EXPLICIT_NAME=bjoern-metwork download uncompress
 	wget -O build/bjoern-metwork/http-parser/http_parser.c "https://raw.githubusercontent.com/nodejs/http-parser/1786fdae36d3d40d59463dacab1cfb4165cf9f1d/http_parser.c"
 	wget -O build/bjoern-metwork/http-parser/http_parser.h "https://raw.githubusercontent.com/nodejs/http-parser/1786fdae36d3d40d59463dacab1cfb4165cf9f1d/http_parser.h"
