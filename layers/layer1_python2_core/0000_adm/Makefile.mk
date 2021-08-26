ab773ce356328bec67bea2a984ff399d4755c029
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-08-26 12:51:53 +0200

    build: kind of git alias for centos8

--- layers/layer1_python2_core/0000_adm/Makefile.mk
+++ layers/layer1_python2_core/0000_adm/Makefile.mk
@@ -1,4 +1,4 @@
-BINS=freeze_requirements download_compile_requirements install_requirements unsafe_pip python3_wrapper
+BINS=freeze_requirements download_compile_requirements install_requirements unsafe_pip python3_wrapper git
 TO_WRAP=plugin_wrapper plugins.list plugins.info plugins.hash plugins.check _plugins.init _plugins.make _plugins.develop plugins.install plugins.uninstall plugins_validate_name
 TO_WRAP_TARGETS=$(addprefix $(PREFIX)/bin/,$(TO_WRAP))
 
@@ -26,5 +26,8 @@ install_requirements: $(MFEXT_HOME)/opt/python3_core/bin/install_requirements
 	ln -s $< $@
 
 unsafe_pip: $(MFEXT_HOME)/opt/python3_core/bin/unsafe_pip
+	ln -s $< $@
+
+git: $(MFEXT_HOME)/opt/python3_core/bin/git
 	ln -s $< $@
 
